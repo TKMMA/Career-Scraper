5f29ad74b6ad8ef97d8b2c0741a805204601d895
TKMMA <tk85@hawaii.edu>
2026-02-23 12:20:34 -1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,6 +11,7 @@ def get_driver():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
     return webdriver.Chrome(options=chrome_options)
 
 def scrape_civil_service():
